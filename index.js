2cad2995f4b9d8284f50545f6e25a19a1ab14611
Swathi M <swathi_m@intuit.com>
2017-09-27 10:26:52 +0530

    Simple post

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send({
+"speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "DuckDuckGo"
 });
+});
+
+
+app.post('/', function(request, response) {
+console.log(request);
+  response.send({
+"speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "DuckDuckGo"
+});
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
